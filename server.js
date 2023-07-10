c37ed9a15d85687a422ac7bff55e7952b622231c
Duy Le <lehongduy.bk@gmail.com>
2023-07-10 07:55:38 +0700

    update POST method

--- server.js
+++ server.js
@@ -2,19 +2,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('CP17301 - Android Networking')
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.post('/rectangle_POST', function (req, res) {
+  var chieudai = req.body.chieudai;
+  var chieurong = req.body.chieurong;
+
+  var chuvi = (chieudai + chieurong) * 2;
+  var dientich = chieudai * chieurong;
+
+  let kq = `Chu vi: ${chuvi} , dien tich: ${dientich}`;
+
+  res.send(kq);
 });
 
+
+
+app.get('/', (req, res) => {
+  res.send('CP17301 - Android Networking')
+});
+
+
+
 app.get("/get_data", async (request, response) => {
-    response.send(`{
+  response.send(`{
         "albumId": 96,
         "id": 4780,
-        "title": "delectus quis reprehenderit ut",
+        "title": "100 bai hat hay nhat",
         "url": "https://via.placeholder.com/600/fef7a0",
         "thumbnailUrl": "https://via.placeholder.com/150/fef7a0"
       }`);
-  
+
 });
 
 
@@ -22,7 +44,7 @@ app.get("/add_data", async (request, response) => {
 
   console.log(request.body);
 
-  
+
 });
 
 
